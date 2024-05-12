877f80d7b6ea9c582f468063ac51164418b2a4d4
Stefan Hoffmann <stho32github@gmail.com>
2024-05-12 09:02:32 +0200

    Add file-or-folder collection

---
+++ Source/Pfs/Pfs.BL.Tests/Syncing/FileOrFolderCollectionTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Pfs.BL.Syncing;
+
+namespace Pfs.BL.Tests.Syncing;
+
+[TestFixture]
+public class FileOrFolderCollectionTests
+{
+    [Test]
+    public void When_Single_Item_Is_Added_Then_Collection_Contains_That_Item()
+    {
+        var collection = new FileOrFolderCollection();
+        var item = new FileOrFolder(true, "testAbsolutePath", "testRelativePath");
+
+        collection.Add(item);
+
+        Assert.That(collection.Items, Contains.Item(item));
+    }
+
+    [Test]
+    public void When_Multiple_Items_Are_Added_Then_Collection_Contains_Those_Items()
+    {
+        var collection = new FileOrFolderCollection();
+        var items = new List<FileOrFolder>
+        {
+            new(true, "testAbsolutePath1", "testRelativePath1"),
+            new(false, "testAbsolutePath2", "testRelativePath2")
+        };
+
+        collection.AddRange(items);
+
+        Assert.That(collection.Items, Is.EquivalentTo(items));
+    }
+
+}
---
+++ Source/Pfs/Pfs.BL/Syncing/FileOrFolderCollection.cs
@@ -0,0 +1,18 @@
+﻿namespace Pfs.BL.Syncing;
+
+public class FileOrFolderCollection
+{
+    private List<FileOrFolder> items = new();
+
+    public void Add(FileOrFolder item)
+    {
+        items.Add(item);
+    }
+
+    public void AddRange(IEnumerable<FileOrFolder> collection)
+    {
+        items.AddRange(collection);
+    }
+
+    public IReadOnlyList<FileOrFolder> Items => items.AsReadOnly();
+}
